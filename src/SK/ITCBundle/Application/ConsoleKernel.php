1da580ed48ad6837df5def8759bb6fe74bfa63fd
Slavomir Kuzma <slavomir.kuzma@gmail.com>
2015-12-25 11:51:12 +0100

    Console Application Kernel added.

---
+++ src/SK/ITCBundle/Application/ConsoleKernel.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * SK ITC Bundle Application Console Kernel
+ *
+ * @author Slavomir Kuzma <slavomir.kuzma@gmail.com>
+ */
+namespace SK\ITCBundle\Application;
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use SK\ITCBundle\SKITCBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Component\DependencyInjection\Variable;
+
+class ConsoleKernel extends Kernel
+{
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \Symfony\Component\HttpKernel\KernelInterface::registerBundles()
+	 */
+	public function registerBundles()
+	{
+		$bundles = array(
+			new FrameworkBundle(),
+			new SKITCBundle(),
+			new MonologBundle()
+		);
+
+		if( in_array( $this->getEnvironment(), array(
+			'dev',
+			'test'
+		), true ) )
+		{
+			// $bundles[] =
+		}
+
+		return $bundles;
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \Symfony\Component\HttpKernel\Kernel::getRootDir()
+	 */
+	public function getRootDir()
+	{
+		return __DIR__.'/../../../..';
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \Symfony\Component\HttpKernel\Kernel::getCacheDir()
+	 */
+	public function getCacheDir()
+	{
+		return $this->getRootDir() . '/cache/' . $this->getEnvironment();
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \Symfony\Component\HttpKernel\Kernel::getLogDir()
+	 */
+	public function getLogDir()
+	{
+		return $this->getRootDir() . '/logs';
+	}
+
+	/**
+	 *
+	 * @param LoaderInterface $loader
+	 */
+	public function registerContainerConfiguration( LoaderInterface $loader )
+	{
+		$environment = $this->getEnvironment();
+		$config = dirname(__DIR__) . sprintf( '/../Resources/config/%s/config.xml', $environment );
+
+		if( ! file_exists( $config ) )
+		{
+			$config = dirname(__DIR__) . sprintf( '/../Resources/config/config.xml' );
+		}
+
+		if( file_exists( $config ) )
+		{
+			$loader->load( $config );
+		}
+	}
+}

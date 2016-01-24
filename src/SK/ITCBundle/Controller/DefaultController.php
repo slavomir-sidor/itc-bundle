9f57a0e8bc7ba79fa7f21b7e417bc3c338df89c9
Slavomir <slavomir@synapse.sk>
2016-01-24 16:33:51 +0100

    ITC FPM REST Updates.

--- src/SK/ITCBundle/Controller/DefaultController.php
+++ src/SK/ITCBundle/Controller/DefaultController.php
@@ -4,7 +4,7 @@
  * SK ITC Bundle Default Controller
  *
  * @licence GNU GPL
- * 
+ *
  * @author Slavomir Kuzma <slavomir.kuzma@gmail.com>
  */
 namespace SK\ITCBundle\Controller;
@@ -14,33 +14,21 @@ class DefaultController extends FilesystemController
 
 	public function indexAction()
 	{
-
 		return $this->render( 'SKITCBundle:Default:index.html.twig', $this->getModel() );
-	
 	}
 
-	public function toolbarAction( 
-		$path = NULL )
+	public function toolbarAction( $path = NULL )
 	{
-
 		return $this->render( 'SKITCBundle:Default:toolbar.html.twig', $this->getModel() );
-	
 	}
 
-	public function workspaceAction( 
-		$path = NULL )
+	public function workspaceAction( $path = NULL )
 	{
-
 		return $this->render( 'SKITCBundle:Default:workspace.html.twig', $this->getModel() );
-	
 	}
 
-	public function eastPaneAction( 
-		$path = NULL )
+	public function eastPaneAction( $path = NULL )
 	{
-
 		return $this->render( 'SKITCBundle:Default:eastPane.html.twig', $this->getModel() );
-	
 	}
-
 }

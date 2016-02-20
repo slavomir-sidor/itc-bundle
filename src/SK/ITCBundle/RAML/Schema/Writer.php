56d94881fcb80bf5b76e59034b30ebd34cb9c0fb
Slavomir <slavomir@synapse.sk>
2016-02-20 17:11:54 +0100

    RAML Updates configuration.

---
+++ src/SK/ITCBundle/RAML/Schema/Writer.php
@@ -0,0 +1,15 @@
+<?php
+namespace SK\ITCBundle\RAML\Schema;
+
+use Raml\ApiDefinition;
+
+class Writer
+{
+
+	public function __construct(){}
+
+	public function write( ApiDefinition $apiDefinition, $filename )
+	{
+
+	}
+}

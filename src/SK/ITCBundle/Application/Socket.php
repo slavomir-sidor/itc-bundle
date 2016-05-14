10412623dbf0c32453ab48f4a5a8a52dd00b7fc4
Slavomir <slavomir@synapse.sk>
2016-05-14 13:43:15 +0200

    Socket updates.

--- src/SK/ITCBundle/Application/Socket.php
+++ src/SK/ITCBundle/Application/Socket.php
@@ -10,8 +10,10 @@ namespace SK\ITCBundle\Application;
 
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class Socket implements MessageComponentInterface
+class Socket extends Application implements MessageComponentInterface
 {
 
 	/**
@@ -89,5 +91,55 @@ class Socket implements MessageComponentInterface
 		echo "An error has occurred: {$e->getMessage()}\n";
 
 		$conn->close();
+	}
+
+	/**
+	 *
+	 * @var string
+	 */
+	private static $logo = '
+
+IIIIIIIIIITTTTTTTTTTTTTTTTTTTTTTT       CCCCCCCCCCCCC
+I::::::::IT:::::::::::::::::::::T    CCC::::::::::::C
+I::::::::IT:::::::::::::::::::::T  CC:::::::::::::::C
+II::::::IIT:::::TT:::::::TT:::::T C:::::CCCCCCCC::::C
+  I::::I  TTTTTT  T:::::T  TTTTTTC:::::C       CCCCCC
+  I::::I          T:::::T       C:::::C
+  I::::I          T:::::T       C:::::C
+  I::::I          T:::::T       C:::::C
+  I::::I          T:::::T       C:::::C
+  I::::I          T:::::T       C:::::C
+  I::::I          T:::::T       C:::::C
+  I::::I          T:::::T        C:::::C       CCCCCC
+II::::::II      TT:::::::TT       C:::::CCCCCCCC::::C
+I::::::::I      T:::::::::T        CC:::::::::::::::C
+I::::::::I      T:::::::::T          CCC::::::::::::C
+IIIIIIIIII      TTTTTTTTTTT             CCCCCCCCCCCCC
+                                                     ';
+
+	/**
+	 * Constructs SK ITC Bundle Application Console
+	 *
+	 * @param KernelInterface $kernel
+	 * @param string $name
+	 * @param string $version
+	 */
+	public function __construct( KernelInterface $kernel, $name = 'ITCloud Console', $version = '${project.version}' )
+	{
+		parent::__construct( $kernel );
+
+		$this->setName( $name );
+		$this->setVersion( $version );
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \Symfony\Component\Console\Application::getHelp()
+	 */
+	public function getHelp()
+	{
+		return self::$logo . parent::getHelp();
 	}
 }
